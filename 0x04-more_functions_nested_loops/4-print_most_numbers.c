d60185a0733c0cc0633561d6faa4f0485e62f621
Paulimhom <Paulgeoph@gmail.com>
2023-03-23 14:34:22 -0700

    Write a function that prints the numbers, from 0 to 9, followed by a new line.
    Prototype: void print_most_numbers(void);
    Do not print 2 and 4
    You can only use _putchar twice in your code

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - do not print 2 and 4
+ *
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
+	}
+
+	_putchar('\n');
+}
