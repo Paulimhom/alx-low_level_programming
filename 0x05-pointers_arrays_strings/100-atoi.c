f297c509cded945133e1fcb0735c14f5bce1d91d
Paulgeop <95384015+Paulimhom@users.noreply.github.com>
2023-04-02 18:04:04 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,44 +17,33 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	while (s[i] == ' ')
+	while (s[c])
 	{
-	i++;
-	}
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
 
-	if (s[i] == '-')
-	{
-	sign = -1;
-	i++;
-	}
-	else if (s[i] == '+')
-	{
-	i++;
-	}
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-	if (result > INT_MAX / 10 ||
-			(result == INT_MAX / 10 &&
-			 s[i] - '0' > INT_MAX % 10))
-	{
-	return (0);
-	}
-	result = result * 10 + (s[i] - '0');
-	i++;
-	}
-	while (s[i] != '\0')
-	{
-	if (s[i] < '0' || s[i] > '9')
-	{
-	return (0);
-	}
-	i++;
+		if (isi == 1)
+		{
+			break;
+		}
+
+		c++;
 	}
 
-	return (sign * result);
+	ni *= min;
+	return (ni);
 }
