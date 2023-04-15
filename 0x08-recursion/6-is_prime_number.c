2bfd60254c45b98768282977ca9b25ce7d96ec93
Paulimhom <Paulgeoph@gmail.com>
2023-04-15 10:29:25 -0700

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0.
    Prototype: int is_prime_number(int n);

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * is_prime_number_helper - Checks if number a prime number
+ * @n: The integer to check for primality
+ * @i: value to help check primality
  *
- * Return: Always 0.
+ * Return: 1 if n is prime, 0 otherwise
  */
-int main(void)
-{
-    int r;
 
-    r = is_prime_number(1);
-    printf("%d\n", r);
-    r = is_prime_number(1024);
-    printf("%d\n", r);
-    r = is_prime_number(16);
-    printf("%d\n", r);
-    r = is_prime_number(17);
-    printf("%d\n", r);
-    r = is_prime_number(25);
-    printf("%d\n", r);
-    r = is_prime_number(-1);
-    printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
-    printf("%d\n", r);
-    return (0);
+int is_prime_number_helper(int n, int i)
+{
+	if (n <= 2)
+	{
+		if (n == 2)
+		{
+		return (1);
+		}
+		else
+		{
+		return (0);
+		}
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	if (i * i > n)
+	{
+		return (1);
+		}
+	return (is_prime_number_helper(n, i + 1));
+}
+
+/**
+ * is_prime_number - Checks if number a prime number
+ * @n: The integer to check for primality
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime_number_helper(n, 2));
 }
